82d486f7a282e96dbab467830f228752650f96f5
Bhuppi <dhamibhupendra3@gmail.com>
2024-04-13 13:09:50 +0530

    saving in local

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -7,26 +7,42 @@ use App\Models\Vendor_details;
 
 class VendorController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('signup');
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         echo '<pre>';
         print_r($request->all());
 
-        $vendor=new Vendor_details();
+        $vendor = new Vendor_details();
 
-        $vendor->id=$request['id'];
-        $vendor->name=$request['name'];
-        $vendor->phone=$request['phone'];
-        $vendor->email=$request['email'];
-        $vendor->store_name=$request['store_name'];
-        $vendor->address=$request['address'];
-        $vendor->gstin=$request['gstin'];
-       
-        $string_version = implode(',', $request['storephotos']);
-        $vendor->storephotos=$string_version;
+        $vendor->id = $request['id'];
+        $vendor->name = $request['name'];
+        $vendor->phone = $request['phone'];
+        $vendor->email = $request['email'];
+        $vendor->store_name = $request['store_name'];
+        $vendor->address = $request['address'];
+        $vendor->gstin = $request['gstin'];
+
+        // Process and store the uploaded images
+        $imagePaths = [];
+        if ($request->hasFile('storephotos')) {
+            foreach ($request->file('storephotos') as $image) {
+                // Generate a unique filename to prevent overwriting
+                $filename = uniqid() . '.' . $image->getClientOriginalExtension();
+                // Move the uploaded image to the storage directory
+                $image->move(public_path('storage/images'), $filename);
+                // Store the path to the image in the array
+                $imagePaths[] = 'storage/images/' . $filename;
+            }
+        }
+
+        // Save the paths to the images in the database
+        $vendor->storephotos = json_encode($imagePaths); // Store the paths as JSON
+
 
         $vendor->save();
 
